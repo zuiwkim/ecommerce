ebaf2cbaa75abd4c8809fa7d979b7334ec93716a
heojae <hubtwork@29cm.co.kr>
2024-12-27 00:41:25 +0900

    [INIT] project setup

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+fun getGitHash(): String {
+	return providers.exec {
+		commandLine("git", "rev-parse", "--short", "HEAD")
+	}.standardOutput.asText.get().trim()
+}
+
+group = "kr.hhplus.be"
+version = getGitHash()
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+	}
+}
+
+dependencies {
+    // Spring
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // DB
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:mysql")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	systemProperty("user.timezone", "UTC")
+}
---
+++ src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
@@ -0,0 +1,34 @@
+package kr.hhplus.be.server;
+
+import jakarta.annotation.PreDestroy;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Configuration
+class TestcontainersConfiguration {
+
+	public static final MySQLContainer<?> MYSQL_CONTAINER;
+
+	static {
+		MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+			.withDatabaseName("hhplus")
+			.withUsername("test")
+			.withPassword("test");
+		MYSQL_CONTAINER.start();
+
+		System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
+		System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
+		System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+		if (MYSQL_CONTAINER.isRunning()) {
+			MYSQL_CONTAINER.stop();
+		}
+	}
+}
