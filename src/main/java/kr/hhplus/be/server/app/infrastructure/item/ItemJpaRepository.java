11874faaffa0fdc51cfddc405d2a271eea29e3df
minju kim <ok200als@naver.com>
2025-01-10 04:25:11 +0900

    [FEAT] 상품 조회 기능 추가

--- src/main/java/kr/hhplus/be/server/app/infrastructure/item/ItemJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.hhplus.be.server.app.infrastructure.item;
-
-import kr.hhplus.be.server.app.domain.item.Items;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemJpaRepository extends JpaRepository<Items, Long> {
-}
