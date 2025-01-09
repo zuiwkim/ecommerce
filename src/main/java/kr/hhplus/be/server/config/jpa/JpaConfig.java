449e2d5f817f39851832bf9285e7648b01f62afa
minju kim <ok200als@naver.com>
2025-01-10 04:29:24 +0900

    [DELETE] 불필요한 파일 삭제

---
+++ src/main/java/kr/hhplus/be/server/config/jpa/JpaConfig.java
@@ -0,0 +1,19 @@
+package kr.hhplus.be.server.config.jpa;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories
+public class JpaConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
+}
